7381be420d3bd590486bcf707b619c40687d8e35
Ruslan <zhd.ruslan@gmail.com>
2017-05-10 22:09:37 +0300

    [getters and setters]
    add getters and setters

--- app/src/main/java/com/zhdan/criminalintent/Crime.java
+++ app/src/main/java/com/zhdan/criminalintent/Crime.java
@@ -9,4 +9,16 @@ public class Crime {
     public Crime() {
         mId = UUID.randomUUID();
     }
+
+    public UUID getmId() {
+        return mId;
+    }
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public void setmTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
 }

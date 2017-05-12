7d327f4a90ac087f7b32a1df7bfd426f0dc48be0
Ruslan <zhd.ruslan@gmail.com>
2017-05-12 20:59:11 +0300

    [upd Crime class]
    model impr

--- app/src/main/java/com/zhdan/criminalintent/Crime.java
+++ app/src/main/java/com/zhdan/criminalintent/Crime.java
@@ -1,13 +1,17 @@
 package com.zhdan.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getmId() {
@@ -20,5 +24,21 @@ public class Crime {
 
     public void setmTitle(String mTitle) {
         this.mTitle = mTitle;
+    }
+
+    public Date getmDate() {
+        return mDate;
+    }
+
+    public void setmDate(Date mDate) {
+        this.mDate = mDate;
+    }
+
+    public boolean ismSolved() {
+        return mSolved;
+    }
+
+    public void setmSolved(boolean mSolved) {
+        this.mSolved = mSolved;
     }
 }

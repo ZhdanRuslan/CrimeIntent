93bff0c458e197a14f57f4b359897502160695af
Ruslan <zhd.ruslan@gmail.com>
2017-05-13 21:23:29 +0300

    [date format]
    format date on button

--- app/src/main/java/com/zhdan/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/zhdan/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,8 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+
+import java.util.Date;
 
 
 public class CrimeFragment extends Fragment {
@@ -48,7 +51,10 @@ public class CrimeFragment extends Fragment {
         });
 
         mDateButton = (Button) v.findViewById(R.id.crime_date);
-        mDateButton.setText(mCrime.getmDate().toString());
+
+        String date = DateFormat.format("EEE, MMM d, yyyy", new Date()).toString();
+
+        mDateButton.setText(date);
         mDateButton.setEnabled(false);
 
         mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);

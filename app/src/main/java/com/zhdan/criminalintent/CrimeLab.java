6b51439024887788c4a707bb303ef150aeae715c
Ruslan <zhd.ruslan@gmail.com>
2017-05-15 07:48:00 +0300

    [CrimeLab implementation]

--- app/src/main/java/com/zhdan/criminalintent/CrimeLab.java
+++ app/src/main/java/com/zhdan/criminalintent/CrimeLab.java
@@ -2,13 +2,32 @@ package com.zhdan.criminalintent;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 class CrimeLab {
     private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
 
     private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
     }
 
-    public static CrimeLab getsCrimeLab(Context context) {
+    public List<Crime> getCrimes(){
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id){
+        for (Crime crime : mCrimes){
+            if (crime.getmId().equals(id)){
+                return crime;
+            }
+        }
+        return null;
+    }
+
+    public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
             sCrimeLab = new CrimeLab(context);
         }

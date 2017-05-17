86844f7e8b3c2196332c81b7260e06b3c0934da4
Ruslan <zhd.ruslan@gmail.com>
2017-05-17 11:59:46 +0300

    [abstarct activity]
    create an abstract activity

--- app/src/main/java/com/zhdan/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/zhdan/criminalintent/SingleFragmentActivity.java
@@ -1,10 +1,27 @@
 package com.zhdan.criminalintent;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
-/**
- * Created by admin on 17.05.2017.
- */
+public abstract class SingleFragmentActivity extends FragmentActivity {
 
-public class SingleFragmentActivity extends FragmentActivity {
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInsranceState){
+        super.onCreate(savedInsranceState);
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null){
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragmentContainer, fragment)
+                    .commit();
+        }
+    }
 }

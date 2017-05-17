3a49c02e5748a59bf09af1630f9371acd828011c
Ruslan <zhd.ruslan@gmail.com>
2017-05-17 11:55:06 +0300

    [add SIngleFragmentActivity class]

---
+++ app/src/main/java/com/zhdan/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,10 @@
+package com.zhdan.criminalintent;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by admin on 17.05.2017.
+ */
+
+public class SingleFragmentActivity extends FragmentActivity {
+}

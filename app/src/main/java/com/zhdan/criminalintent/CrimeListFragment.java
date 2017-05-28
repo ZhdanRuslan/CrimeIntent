8164110453a6cf64dd40e90169d999994ddd19b0
Ruslan <zhd.ruslan@gmail.com>
2017-05-28 14:36:52 +0300

    [CrimeListFragment ]
    CrimeListFragment implementation

--- app/src/main/java/com/zhdan/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/zhdan/criminalintent/CrimeListFragment.java
@@ -1,7 +1,23 @@
 package com.zhdan.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeListFragment extends Fragment {
-    /*NOP*/
+    private RecyclerView mCrimeRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
 }

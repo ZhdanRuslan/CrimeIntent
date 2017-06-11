a0c88d4793ae24d162d0912d271e6418ceaf758e
Ruslan <zhd.ruslan@gmail.com>
2017-06-11 21:03:59 +0300

    [Override onResume]

--- app/src/main/java/com/zhdan/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/zhdan/criminalintent/CrimeListFragment.java
@@ -55,9 +55,18 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
+
+        
+
         mAdapter = new CrimeAdapter(crimes);
         mCrimeRecyclerView.setAdapter(mAdapter);
     }

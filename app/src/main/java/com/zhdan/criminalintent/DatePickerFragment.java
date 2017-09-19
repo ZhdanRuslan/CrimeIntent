84d664862ab5366ed23b37da27c683dde0dcfbc5
Ruslan <zhd.ruslan@gmail.com>
2017-09-19 11:10:54 +0300

    [impl]

--- app/src/main/java/com/zhdan/criminalintent/DatePickerFragment.java
+++ app/src/main/java/com/zhdan/criminalintent/DatePickerFragment.java
@@ -3,11 +3,15 @@ package com.zhdan.criminalintent;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 
 public class DatePickerFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState);
+        return new AlertDialog.Builder(getActivity())
+                .setTitle(R.string.date_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
     }
 }

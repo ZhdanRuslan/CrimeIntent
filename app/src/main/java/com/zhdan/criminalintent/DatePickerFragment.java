f2575402e5ff9216477b724ed4a3d53df03646c1
Ruslan <zhd.ruslan@gmail.com>
2017-09-19 11:10:06 +0300

    [add date picker]

---
+++ app/src/main/java/com/zhdan/criminalintent/DatePickerFragment.java
@@ -0,0 +1,13 @@
+package com.zhdan.criminalintent;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class DatePickerFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        return super.onCreateDialog(savedInstanceState);
+    }
+}

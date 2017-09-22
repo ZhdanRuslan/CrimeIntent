418571ea4658090aa6ffafcc42256325c05ae749
Ruslan <zhd.ruslan@gmail.com>
2017-09-22 10:47:19 +0300

    [change parent class]

--- app/src/main/java/com/zhdan/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/zhdan/criminalintent/SingleFragmentActivity.java
@@ -2,22 +2,22 @@ package com.zhdan.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
     @Override
-    public void onCreate(Bundle savedInsranceState){
+    public void onCreate(Bundle savedInsranceState) {
         super.onCreate(savedInsranceState);
         setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
 
-        if (fragment == null){
+        if (fragment == null) {
             fragment = createFragment();
             fm.beginTransaction()
                     .add(R.id.fragmentContainer, fragment)

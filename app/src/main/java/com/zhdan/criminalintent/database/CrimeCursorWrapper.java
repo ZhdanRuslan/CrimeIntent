a94e541ded541d778be3f9dc2e47ddd65124d400
Ruslan <zhd.ruslan@gmail.com>
2017-10-19 09:17:02 +0300

    [add getCrime]

--- app/src/main/java/com/zhdan/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/zhdan/criminalintent/database/CrimeCursorWrapper.java
@@ -3,6 +3,12 @@ package com.zhdan.criminalintent.database;
 import android.database.Cursor;
 import android.database.CursorWrapper;
 
+import com.zhdan.criminalintent.Crime;
+import com.zhdan.criminalintent.database.CrimeDbSchema.CrimeTable;
+
+import java.util.Date;
+import java.util.UUID;
+
 public class CrimeCursorWrapper extends CursorWrapper {
     /**
      * Creates a cursor wrapper.
@@ -11,5 +17,22 @@ public class CrimeCursorWrapper extends CursorWrapper {
      */
     public CrimeCursorWrapper(Cursor cursor) {
         super(cursor);
+    }
+
+
+    /**
+     * Get a Crime entity from DB
+     */
+    public Crime getCrime() {
+        String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+
+        Crime crime = new Crime(UUID.fromString(uuidString));
+        crime.setTitle(title);
+        crime.setDate(new Date(date));
+        crime.setSolved(isSolved != 0);
+        return crime;
     }
 }

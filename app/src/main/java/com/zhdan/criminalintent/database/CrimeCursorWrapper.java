093a26bd5b008142d42d3d484220961e51534ab8
Ruslan <zhd.ruslan@gmail.com>
2017-11-08 09:30:16 +0200

    [Ligging work with DB]

--- app/src/main/java/com/zhdan/criminalintent/database/CrimeCursorWrapper.java
+++ app/src/main/java/com/zhdan/criminalintent/database/CrimeCursorWrapper.java
@@ -2,6 +2,7 @@ package com.zhdan.criminalintent.database;
 
 import android.database.Cursor;
 import android.database.CursorWrapper;
+import android.util.Log;
 
 import com.zhdan.criminalintent.Crime;
 import com.zhdan.criminalintent.database.CrimeDbSchema.CrimeTable;
@@ -25,14 +26,19 @@ public class CrimeCursorWrapper extends CursorWrapper {
      */
     public Crime getCrime() {
         String uuidString = getString(getColumnIndex(CrimeTable.Cols.UUID));
+        Log.e("GET", "getStr = " + uuidString);
         String title = getString(getColumnIndex(CrimeTable.Cols.TITLE));
+        Log.e("GET", "getTitle = " + title);
         long date = getLong(getColumnIndex(CrimeTable.Cols.DATE));
+        Log.e("GET", "getDate = " + date);
         int isSolved = getInt(getColumnIndex(CrimeTable.Cols.SOLVED));
+        Log.e("GET", "getSolved = " + isSolved);
 
         Crime crime = new Crime(UUID.fromString(uuidString));
         crime.setTitle(title);
         crime.setDate(new Date(date));
         crime.setSolved(isSolved != 0);
+
         return crime;
     }
 }

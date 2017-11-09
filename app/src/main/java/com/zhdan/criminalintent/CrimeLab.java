cf543235d7c48edeb782c5f82b3957594e81a0b4
Ruslan <zhd.ruslan@gmail.com>
2017-11-09 09:25:41 +0200

    [add suspect to ConVal]
    #add suspect key-value pair into method which crate ContentValues of Crime entity

--- app/src/main/java/com/zhdan/criminalintent/CrimeLab.java
+++ app/src/main/java/com/zhdan/criminalintent/CrimeLab.java
@@ -99,6 +99,7 @@ class CrimeLab {
         values.put(CrimeTable.Cols.TITLE, crime.getTitle());
         values.put(CrimeTable.Cols.DATE, crime.getDate().getTime());
         values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
+        values.put(CrimeTable.Cols.SUSPECT, crime.getmSuspect());
         return values;
     }
 }

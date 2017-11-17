5500f3607206894ac874d72baa16e823c54583a5
Ruslan <zhd.ruslan@gmail.com>
2017-11-17 10:46:58 +0200

    [rerange]

--- app/src/main/java/com/zhdan/criminalintent/CrimeListActivity.java
+++ app/src/main/java/com/zhdan/criminalintent/CrimeListActivity.java
@@ -3,13 +3,14 @@ package com.zhdan.criminalintent;
 import android.support.v4.app.Fragment;
 
 public class CrimeListActivity extends SingleFragmentActivity {
-    @Override
-    protected Fragment createFragment() {
-        return new CrimeListFragment();
-    }
 
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_twopane;
+    }
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
     }
 }
